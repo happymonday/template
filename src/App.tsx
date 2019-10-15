fcdac34e411994e1f05decdb135ab3deda0e5aa2
Jared Gray <jared@jaredgray.us>
2019-10-15 11:04:20 -0700

    Build sample user form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+interface IUserProps {
+  firstName: string;
+  lastName: string;
+  address: string;
+}
 
 const App: React.FC = () => {
+  const [user, setUser] = useState<IUserProps>({firstName: "", lastName: "", address: ""});
+  const [formSubmitted, setFormSubmited] = useState<boolean>(false);
+
+  const handleSubmit = () => {
+    setFormSubmited(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <label htmlFor="first-name">First Name</label>
+        <input 
+          type="text" 
+          placeholder="First Name" 
+          id="first-name" 
+          value={user.firstName} 
+          onChange={e => setUser({...user, firstName: e.target.value})}
+        />
+        <br/>
+
+        <label htmlFor="last-name">Last Name</label>
+        <input 
+          type="text" 
+          placeholder="Last Name" 
+          id="last-name" 
+          value={user.lastName} 
+          onChange={e => setUser({...user, lastName: e.target.value})}
+        />
+        <br/>
+
+        <label htmlFor="address">Address</label>
+        <input 
+          type="text" 
+          placeholder="Address" 
+          id="address" 
+          value={user.address} 
+          onChange={e => setUser({...user, address: e.target.value})}
+        />
+
+        <button onClick={(e) => {e.preventDefault(); handleSubmit(); }} type="button">Submit</button>
+      </form>
+
+      {formSubmitted ? (
+        <div>
+          <h1>User Details</h1>
+          <p>First Name: {user.firstName}</p>
+          <p>Last Name: {user.lastName}</p>
+          <p>Address: {user.address}</p>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
 
